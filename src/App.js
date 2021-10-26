9bc2413ce2ebaa90a01a38bbd4756940af90f442
maxwilliams361 <52715538+maxwilliams361@users.noreply.github.com>
2021-10-26 18:26:45 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <table>
+        <th>First Name</th>
+        <tr>
+          <td>Name</td>
+        </tr>
+        <tr>
+          <td>Name</td>
+        </tr>
+        <tr>
+          <td>Name</td>
+        </tr>
+        <tr>
+          <td>Name</td>
+        </tr>
+        <tr>
+          <td>Name</td>
+        </tr>
+      </table>
+    </div>
+  );
+}
